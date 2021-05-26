124f43a9c0a7521ddce3126ac7e8e59b43047da9
KsGr3g0ry <gbrothers29@gmail.com>
2021-05-26 16:15:56 -0500

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Typed from "react-typed";
-import Particles from "react-particles-js";
 
 function App() {
   return (
-    <>
-    <Particles 
-      className="particles-canvas"
-      params={{
-        particles: {
-          number: {
-            value: 30,
-            density: {
-              enable: true,
-              value_area: 900
-            }
-          },
-          shape: {
-            type: "circle",
-            stroke: {
-              width: 6,
-              color: "#f9ab00"
-            }
-
-          }
-        }
-      }}/>
-    <Navbar />
-    <Header />
-    </>
-  )
-      
+    <h2>Portfolio Project</h2>
+  );
 }
 
 export default App;
