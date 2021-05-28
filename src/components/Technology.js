d71fee5154359cc474495c38711100c86a8e1412
KsGr3g0ry <gbrothers29@gmail.com>
2021-05-28 07:48:35 -0500

    Services Completed

---
+++ src/components/Technology.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faDesktop, faFileCode } from "@fortawesome/free-solid-svg-icons";
+
+const Technology = () => {
+    return (
+        <div className="technology">
+            <h1 className="py-5">my services</h1>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3 col-sm-6">
+                            <div className="box">
+                            <div className="circle"><FontAwesomeIcon className="icon" icon={faDesktop} size="2x" /> </div>
+                               <h3>Web Design</h3> 
+                               <p>I approach each project individually and always focus on the result.</p>
+                            </div>
+                        </div>
+                        {/* - */}
+                        <div className="col-md-3 col-sm-6">
+                            <div className="box">
+                            <div className="circle"><FontAwesomeIcon className="icon" icon={faFileCode} size="2x" /> </div>
+                               <h3>Web Development</h3> 
+                               <p>Stakeholders applications will be built with new proven technologies.</p>
+                            </div>
+                        </div>
+                        {/* - */}
+                        <div className="col-md-3 col-sm-6">
+                            <div className="box">
+                            <div className="circle"><FontAwesomeIcon className="icon" icon={faFacebookF} size="2x" /> </div>
+                               <h3>Social Media</h3> 
+                               <p>Your potential audience will see your services or products on social media sites.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default Technology;
