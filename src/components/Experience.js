616092972644b645400e423644837947e0f82d9f
KsGr3g0ry <gbrothers29@gmail.com>
2021-05-28 10:07:48 -0500

    Experience Completed

---
+++ src/components/Experience.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const Experience = () => {
+    return (
+        <div className="experience">
+            <div className="d-flex justify-content-center my-5">
+               <h1>experience</h1> 
+            </div>
+            <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2020-Current</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nemo ducimus tempora at 
+                        animi alias quibusdam delectus eum iure nam corrupti officia nihil, est quaerat labore odit 
+                        aliquam voluptatum libero nesciunt non modi repellat doloribus excepturi. Dolorum aliquam 
+                        veniam neque!
+                        </p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2018-2020</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nemo ducimus tempora at 
+                        animi alias quibusdam delectus eum iure nam corrupti officia nihil, est quaerat labore odit 
+                        aliquam voluptatum libero nesciunt non modi repellat doloribus excepturi. Dolorum aliquam 
+                        veniam neque!
+                        </p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2017-2018</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nemo ducimus tempora at 
+                        animi alias quibusdam delectus eum iure nam corrupti officia nihil, est quaerat labore odit 
+                        aliquam voluptatum libero nesciunt non modi repellat doloribus excepturi. Dolorum aliquam 
+                        veniam neque!
+                        </p>
+                    </div>
+                </div>
+                {/* - */}
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker"></div>
+                    <div className="timeline-content">
+                        <h3>2015-2017</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet nemo ducimus tempora at 
+                        animi alias quibusdam delectus eum iure nam corrupti officia nihil, est quaerat labore odit 
+                        aliquam voluptatum libero nesciunt non modi repellat doloribus excepturi. Dolorum aliquam 
+                        veniam neque!
+                        </p>
+                    </div>
+                </div>
+            </div>   
+        </div>
+    )
+}
+
+export default Experience;
